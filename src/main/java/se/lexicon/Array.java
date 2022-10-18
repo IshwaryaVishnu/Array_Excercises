bbe72664b57da562cc6c132201b429a87acf817d
Iswarya Devi Gomathi Sankar <ishu27.v@gmail.com>
2022-10-18 14:34:32 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/Array.java
@@ -0,0 +1,16 @@
+package se.lexicon;
+public class Array {
+
+    public static void main(String[]args) {
+
+        int[]  number = { 11, 23, 39 };
+        for (int elements : number) {
+            System.out.print(elements+" ");
+        }
+    }
+}
+
+
+
+
+
